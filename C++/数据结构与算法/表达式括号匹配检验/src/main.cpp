35f1f0e643b2c9946d996d25c22ec7a72231179d
zhxycn <admin@owo.cab>
2025-10-09 10:42:08 +0800

    数据结构与算法-课程实验2

---
+++ C++/数据结构与算法/表达式括号匹配检验/src/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct symbol {
+    char s;
+    int line;
+    int col;
+};
+
+class Stack {
+    public:
+        vector<symbol> stack;
+
+    [[nodiscard]] bool empty() const {
+        return stack.empty();
+    }
+
+    [[nodiscard]] char top() const {
+        if (stack.empty()) {
+            return '\0';
+        }
+        return stack.back().s;
+    }
+
+    void pop() {
+        stack.pop_back();
+    }
+
+    void push(const char symbol, const int line, const int col) {
+        stack.push_back({symbol, line, col});
+    }
+};
+
+int main() {
+    while (true) {
+        string s;
+        Stack stack;
+        int line = 0;
+
+        while (s.back() != '#') {
+            line++;
+            getline(cin, s);
+
+            if (s == "@") {
+                cout << "Exit..." << endl;
+                return 0;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                if (stack.top() == '{' && s.at(i) == '}' ||
+                    stack.top() == '[' && s.at(i) == ']' ||
+                    stack.top() == '(' && s.at(i) == ')') {
+                    stack.pop();
+                    continue;
+                    }
+
+                if (s.at(i) == '{' || s.at(i) == '[' || s.at(i) == '(' ||
+                    s.at(i) == '}' || s.at(i) == ']' || s.at(i) == ')') {
+                    stack.push(s.at(i), line, i + 1);
+                    }
+            }
+        }
+
+        if (stack.empty()) {
+            cout << "All symbols match" << endl;
+        } else {
+            for (const auto& sym : stack.stack) {
+                cout << "Unmatched '" << sym.s << "' at line " << sym.line << ", col " << sym.col << endl;
+            }
+        }
+
+        cout << endl;
+    }
+}
